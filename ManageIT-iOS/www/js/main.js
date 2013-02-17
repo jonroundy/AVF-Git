1843c2da46be10b4ab979b2fe37fe241cfe46ab0
Jon Roundy <jonroundy@fullsail.edu>
2013-02-16 23:01:41 -0600

    Created JS link injection function
    Wrote a function to apply css based on device orientation on load. It
    also updates the css file bases on orientation change.
    ~ Finally have time this weekend to get some stuff done. ~
    Trying to keep up with school and work is becoming quite diffuclt..

---
+++ ManageIT-iOS/www/js/main.js
@@ -0,0 +1,22 @@
+//Inject CSS file on load and orientation change
+
+function orient() {
+	if (Math.abs(window.orientation) == 90) {
+		$("#orientation").each(function(){
+	  	$(this).attr('href','css/landscape.css');		
+	  });
+	}
+	else { 
+		$("#orientation").each(function(){
+	  	$(this).attr('href','css/portrait.css');		
+	  }); 
+	}
+};
+
+$(window).on('ready', function(){
+	orient();
+});
+
+$(window).on('orientationchange', function(){
+	orient();
+});
