fb95734ec9509f8eda5cd388e50dbabd774ec117
Jon Roundy <jonroundy@fullsail.edu>
2013-02-28 22:40:24 -0600

    Issues in git
    Lost half of todays work trying to figure out what going on with repo.
    Quicktime not converting video ....

--- ManageIT-iOS/www/js/main.js
+++ ManageIT-iOS/www/js/main.js
@@ -1,9 +1,16 @@
 // Clear out page content and load clicked link
-
 $("#native").click( function() {
   //alert("Handler for .click() called.");
   $('.content').fadeOut(500, function() {
   $('.content').load('home.html').show();
+});
+return false;
+});
+
+$("#camera").click( function() {
+  //alert("Handler for .click() called.");
+  $('.content').fadeOut(500, function() {
+  $('.content').load('camera.html').show();
 });
 return false;
 });
@@ -85,15 +92,13 @@ $('.content').fadeIn(1000);
 	return false;
 });
 
-
-
 $("#facebook").on('click', function(){
 $('.content').fadeOut(500);
 $('.content').empty();
 $.ajax({
-url: "https://graph.facebook.com/search?q=tech&type=post",
+url: "https://graph.facebook.com/search?q=coffee&type=place&center=37.76,-122.427&distance=1000&access_token=AAAAAAITEghMBAOaFKMmoY4zE4JX9LcPI5O2aBZANoWSDRUhcnvMU0QikM8rqKn4I7IKqvQ9XLDqB6YdoZAW3FjCRAvbXSBUkmmo4Of0gZAFfYFK5yfx",
 type: 'GET',
-dataType: 'json',
+dataType: 'jsonp',
 success: function(request){
 console.log(request);
 for (i=0; i<3; i++){
@@ -101,9 +106,13 @@ $(
 			"<br/>"+
 			"<ul>"+ 
 			"<li>"+
-			"<p>" + request.data[i].from.name+"</p> <br/>" +
-			"<p>" +	request.data[i].message +",</p>" + 
-			"<p>" +	request.data[i].created_time +"</p>" +
+			"<p>" + 'Name: ' + request.data[i].name+"</p> <br/>" +
+			"<p>" + 'Street: ' +	request.data[i].location.street +",</p>" + 
+			"<p>" + 'City: ' +	request.data[i].location.city +"</p>" +
+			"<p>" + 'State: ' +	request.data[i].location.state +"</p>" +
+			"<p>" + 'Zip: ' +	request.data[i].location.zip +"</p>" +
+			"<p>" + 'Latitude: ' +	request.data[i].location.latitude +"</p>" +
+			"<p>" + 'Longitude: ' +	request.data[i].location.longitude +"</p>" +
 			"</li>" +
 			"<br/>" +
 			"</ul>"
--- ManageIT-iOS/www/js/native/geo.js
+++ ManageIT-iOS/www/js/native/geo.js
@@ -1,24 +1,75 @@
 document.addEventListener("deviceready", onDeviceReady, false);
-alert("GEO READY");
+	
+			//alert("GEO READY");
+	//var watchID = null;
 
-function onDeviceReady() {
-	 navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	function onDeviceReady() {
+		var options = { enableHighAccuracy: true };
+		navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
 }
+
     function onSuccess(position) {
+        
         var element = document.getElementById('geoLocation');
         element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + position.timestamp          		   + '<br />';
+                            'Timestamp: '          + position.timestamp          		   + '<br />' +
+                            '<br />';
+		
+			placeMap();
     }
 
+	var placeMap = function(){
+		var mapOptions = {
+          center: new google.maps.LatLng(34.014264, -86.006639),
+          zoom: 8,
+          mapTypeControl : false, 
+          navigationControl : false,
+          streetViewControl : false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+            sites();
+            //setMarkers(map, sites);
+            
+};
+
+var sites = function(request) {
+$.ajax({
+url: "https://graph.facebook.com/search?q=restaurant&type=place&center=34.014264,-86.006639&distance=1000&access_token=AAAAAAITEghMBAMFdsgvFm1nadPyrg2DK3i0DDIaWjZAUiWM7hv9g8ycDZCZCuJzv2G78aW6G3GwMxspLj0Dy49ZB2xRo1TcsOz6dBNTujhlAJLtlnwYx",
+type: 'GET',
+dataType: 'json',
+success: function(request){
+for (i=0; i<3; i++) {
+	
+	var latVal = request.data[i].location.latitude;
+	var lngVal = request.data[i].location.longitude;
+	var resName = request.data[i].name;
+			}
+			console.log(request);
+		}
+	});
+};
+
+/*var setMarkers = function(map, data) {
+alert('setmarkers');
+console.log(data[i].location.latitude);
+for (var i=0; i<5; i++) {
+    //var sites = locations[i];
+    var myLatLng = new google.maps.LatLng(data[i].location.latitude, data[i].location.longitude);
+    var marker = new google.maps.Marker({position: myLatLng, map: map, title: data[i].name});
+			}
+};
+*/
+    
+    
+    
+    
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+	    navigator.notification.alert(
+            "Can't get your current location. Make sure the geolocation service is enabled for this app.");
     }
